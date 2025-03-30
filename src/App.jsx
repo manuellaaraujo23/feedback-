bcce995e726f7505f9000b4049018029b11de51b
manuchasilva <manuchasilva@gmail.com>
2025-03-30 17:54:05 +0000

    inicio da estilização do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import iconStar from "./assets/icon-star.svg"
+import illustrationThankYou from "./assets/illustration-thank-you.svg"
 import { useState } from "react"
 
 export function App(){
@@ -22,8 +23,8 @@ export function App(){
  
 
   return(
-    <div className="h-svh bg-very-dark-blue text-white flex justify-center items-center px-6">
-     {submited === false ?(
+  
+     submited === false ?(
        <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass">
         
        <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
@@ -45,8 +46,13 @@ export function App(){
 
      </div>
      ) : (
-       <p>Outro Código</p>
-     )}
-    </div>
+      <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass"> 
+          <img src={illustrationThankYou} alt="illustration thank you" />
+       
+          <p></p>
+
+      </div>
+     )
+  
   )
 }
