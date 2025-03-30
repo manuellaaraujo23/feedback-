5f9b14b193407532f4d2c7dd63a912cb7d783858
manuchasilva <manuchasilva@gmail.com>
2025-03-30 18:28:02 +0000

    Ajustes dos design responsivo e FIM do projeto.

--- src/App.jsx
+++ src/App.jsx
@@ -35,14 +35,14 @@ export function App(){
        <p className="text-light-grey text-sm leading-1 mb-6">Please let us know how we did your suport request. All feedback is appreciated to help us improve our offering!</p>
 
        <div className="flex justify-between mb-6">
-         <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5  rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white"onClick={()=>handleChangeRateNote(1)}/>
-         <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5  rounded-full text-sm text-medium-grey font-bold  focus:bg-medium-grey  focus:text-white"onClick={()=>handleChangeRateNote(2)}/>
-         <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5  rounded-full text-sm text-medium-grey font-bold  focus:bg-medium-grey  focus:text-white"onClick={()=>handleChangeRateNote(3)}/>
-         <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5  rounded-full text-sm text-medium-grey font-bold  focus:bg-medium-grey  focus:text-white"onClick={()=>handleChangeRateNote(4)}/>
-         <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5  rounded-full text-sm text-medium-grey font-bold  focus:bg-medium-grey  focus:text-white"onClick={()=>handleChangeRateNote(5)}/>
+         <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5  rounded-full text-sm text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange  hover:text-white  cursor-pointer sm:w-1-sm:h-13 transition duration-400" onClick={()=>handleChangeRateNote(1)}/>
+         <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5  rounded-full text-sm text-medium-grey font-bold  focus:bg-medium-grey  focus:text-white hover:bg-orange   hover:text-white cursor-pointer sm:w-1-sm:h-13 transition duration-400" onClick={()=>handleChangeRateNote(2)}/>
+         <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5  rounded-full text-sm text-medium-grey font-bold  focus:bg-medium-grey  focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-1-sm:h-13 transition duration-400" onClick={()=>handleChangeRateNote(3)}/>
+         <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5  rounded-full text-sm text-medium-grey font-bold  focus:bg-medium-grey  focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-1-sm:h-13 transition duration-400" onClick={()=>handleChangeRateNote(4)}/>
+         <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5  rounded-full text-sm text-medium-grey font-bold  focus:bg-medium-grey  hover:bg-orange  focus:text-white hover:text-white cursor-pointer sm:w-1-sm:h-13 transition duration-400" onClick={()=>handleChangeRateNote(5)}/>
        </div>
 
-      <button className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1" onClick={handleSubmit}>SUBMIT</button>
+      <button className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1 hover:bg-white hover:text-orange transition duration-400" onClick={handleSubmit} >SUBMIT</button>
 
      </div>
      ) : (
